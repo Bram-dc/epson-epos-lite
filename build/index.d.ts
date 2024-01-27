b750525ee2d45ff526a9e91e4ca4f83d8ce97911
Bram del Canho <bramdelcanho@gmail.com>
2024-01-27 14:55:23 +0100

    Delete unnecessary files

--- build/index.d.ts
+++
@@ -1,9 +0,0 @@
-import Builder from './Builder';
-import HTTPSPrinter from './HTTPSPrinter';
-import USBPrinter from './USBPrinter';
-declare const ePOS: {
-    Builder: typeof Builder;
-    HTTPSPrinter: typeof HTTPSPrinter;
-    USBPrinter: typeof USBPrinter;
-};
-export default ePOS;
